886bf85716752ceaf235864c477ac2e0ddbc90eb
bucklan <as1kenjebaev@gmail.com>
2023-07-14 16:14:59 +0600

    little fix

--- routes/api/admin/user/clients.php
+++ routes/api/admin/user/clients.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\Admin\User\Client as Client;
 Route::prefix('clients')->group(function () {
     Route::get('datatable', [Client\ClientController::class, 'datatable']);
     Route::post('check-access', [Client\ClientController::class, 'checkAccess']);
+
 });
