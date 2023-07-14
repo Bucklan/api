96b2f68c601fde3432d3209de843833747fdd399
bucklan <as1kenjebaev@gmail.com>
2023-07-14 16:08:40 +0600

    little fix

--- routes/api/admin/user/clients.php
+++ routes/api/admin/user/clients.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\User\Client as Client;
-Route::prefix('clients')->group(function () {
-    Route::get('datatable',[Client\ClientController::class,'datatable']);
-    Route::post('check-access', [Client\ClientController::class, 'checkAccess']);
 
+Route::prefix('clients')->group(function () {
+    Route::get('datatable', [Client\ClientController::class, 'datatable']);
+    Route::post('check-access', [Client\ClientController::class, 'checkAccess']);
 });
